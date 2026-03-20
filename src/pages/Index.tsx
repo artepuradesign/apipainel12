43ef9921291c9404cefc672c7ce11e511c2da65e
Arte Pura Design <contato@artepuradesign.com.br>
2026-03-20 06:37:54 -0300

    DELETE

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-  return (
-    <main className="hero-gradient-bg flex min-h-screen items-center justify-center">
-      <h1 className="hero-title text-center">FLAMENGO</h1>
-    </main>
-  );
-};
-
-export default Index;
